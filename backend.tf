ddea0a9d2cab82e4e8f240f410cdc83442c8b40c
egor-kazakov <infernal994@gmail.com>
2023-07-08 16:58:31 +0300

    Added tfstate backend

---
+++ backend.tf
@@ -0,0 +1,10 @@
+terraform {
+  backend "http" {
+    address = "https://s053278.gitlab.yandexcloud.net/api/v4/projects/3/terraform/state/tfstate"
+    lock_address = "https://s053278.gitlab.yandexcloud.net/api/v4/projects/3/terraform/state/tfstate/lock"
+    username = "s053279"
+    password = "f9607592"
+    lock_method = "POST"
+    unlock_method = "DELETE"
+  }
+}
