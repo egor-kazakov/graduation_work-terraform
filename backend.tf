6554272d63bccb243e047af2a935e5a9b5e82380
egor-kazakov <infernal994@gmail.com>
2023-07-08 16:59:48 +0300

    Fix auth backend

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "http" {
     address = "https://s053278.gitlab.yandexcloud.net/api/v4/projects/3/terraform/state/tfstate"
     lock_address = "https://s053278.gitlab.yandexcloud.net/api/v4/projects/3/terraform/state/tfstate/lock"
-    username = "s053279"
+    username = "s053278"
     password = "f9607592"
     lock_method = "POST"
     unlock_method = "DELETE"
