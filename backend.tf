584055b54a0f764c24fa7852c3575921d2c39738
egor-kazakov <infernal994@gmail.com>
2023-07-08 17:30:18 +0300

    Test backend.tf

--- backend.tf
+++ backend.tf
@@ -1,12 +1,4 @@
 terraform {
   backend "http" {
-    address = "https://s053278.gitlab.yandexcloud.net/api/v4/projects/3/terraform/state/tfstate"
-    lock_address = "https://s053278.gitlab.yandexcloud.net/api/v4/projects/3/terraform/state/tfstate/lock"
-    unlock_address = "https://s053278.gitlab.yandexcloud.net/api/v4/projects/3/terraform/state/tfstate/lock"
-    username = "test"
-    password = "glpat-yRdZNGKq9hv6xbAm4LJN"
-    lock_method = "POST"
-    unlock_method = "DELETE"
-    retry_wait_min= "5"
   }
 }
