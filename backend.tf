0e2ce583676b1653f19acd8bef45b9a7723f056b
s053278 <egor.kazakov@sparm.com>
2023-07-18 11:50:01 +0300

    Update comments

--- backend.tf
+++ backend.tf
@@ -1,8 +1,11 @@
 ##########
 # Конфигурационный файл-заглушка для работы с удаленным tfstate в проекте GitLab.
 #
-# Данные передаются как переменные окружения при работе контейнера registry.gitlab.com/gitlab-org/terraform-images/stable:latest
+# Данные передаются как переменные окружения при работе 
+# контейнера registry.gitlab.com/gitlab-org/terraform-images/stable:latest
+#
 # Для применения конфигурации используется команда: gitlab-terraform
+# TF_ADDRESS=${CI_API_V4_URL}/projects/${CI_PROJECT_ID}/terraform/state/yc-tfstate
 ##########
 
 terraform {
