98d75e426ce42c6ebba636e50e06c85f1fd2d4af
s053278 <egor.kazakov@sparm.com>
2023-07-21 21:54:34 +0300

    Repare dns zone

--- networks.tf
+++ networks.tf
@@ -1,6 +1,11 @@
 ##########
 # Конфигурационный файл сети.
-# Описаны сеть и подсети в каждой зоне доступности.
+# Описаны сеть и подсети в каждой зоне доступности,
+# создает DNS-зону для управления DNS-записями.
+#
+# Для работы необходимо настроить у провайдера домена делегирование:
+#   - ns1.yandexcloud.net
+#   - ns2.yandexcloud.net
 ##########
 
 resource "yandex_vpc_network" "this" {
@@ -14,4 +19,12 @@ resource "yandex_vpc_subnet" "this" {
   zone           = var.zone[count.index]
   network_id     = yandex_vpc_network.this.id
   v4_cidr_blocks = var.cidr_blocks[count.index]
+}
+
+resource "yandex_dns_zone" "this" {
+  name        = replace(var.dns_name, ".", "-")
+  description = "Public zone"
+
+  zone    = "${var.dns_name}."
+  public  = true
 }
